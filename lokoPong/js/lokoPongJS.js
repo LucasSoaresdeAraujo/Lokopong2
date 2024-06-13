c3953478425c57aa11776aedacf4e80eb0b94ec0
LucasSoaresdeAraujo <161390682+LucasSoaresdeAraujo@users.noreply.github.com>
2024-06-13 12:07:10 -0300

    Add files via upload

--- lokoPong/js/lokoPongJS.js
+++ lokoPong/js/lokoPongJS.js
@@ -1,7 +1,10 @@
 const canvas = document.querySelector('#canvas')
 const quadro = canvas.getContext('2d')
+const audio = new Audio('js/pong.mp3')
 
 quadro.fillStyle = '#4B0082'
+
+
 let player1 ={
     px:80, //posição no eixo x do jogador 
     py:260, //posição do eixo y do jogador 
@@ -15,8 +18,7 @@ let player2 ={
     py:260,
     tamanho:30,
     largura:200,
-    dir:0,
-  
+    dir:0,  
 }
 
 
@@ -35,8 +37,7 @@ quadro.font = '40px arial'
 let pts1 = 0
 let pts2 = 0
 let jogar = true
-let score1 = quadro.fillText(`score1: ${pts1}` ,250,50)
-let score2 = quadro.fillText(`score2: ${pts2}` ,850,50)
+
 
 
 document.addEventListener('keydown', function(e){
@@ -47,9 +48,6 @@ document.addEventListener('keydown', function(e){
  else if(e.keyCode === 83){
   player1.dir = 8
  }
-
-
-
 })
 
 
@@ -61,11 +59,29 @@ document.addEventListener('keyup', function(e){
  else if(e.keyCode === 83){
   player1.dir = 0
  }
-
-
-
 })
 
+
+document.addEventListener('keydown', function(e){
+  if(e.keyCode === 38){
+  player2.dir = -8
+
+  }
+ else if(e.keyCode ===40){
+  player2.dir = 8
+ }
+})
+
+
+document.addEventListener('keyup', function(e){
+  if(e.keyCode === 38){
+  player2.dir = 0
+
+  }
+ else if(e.keyCode === 40){
+  player2.dir = 0
+ }
+})
 
 
 function moverPlayer1(){
@@ -78,36 +94,6 @@ function moverPlayer1(){
   player1.py += player1.dir
 }
 
-
-document.addEventListener('keydown', function(e){
-  if(e.keyCode === 38){
-  player2.dir = -8
-
-  }
- else if(e.keyCode ===40){
-  player2.dir = 8
- }
-
-
-
-})
-
-
-document.addEventListener('keyup', function(e){
-  if(e.keyCode === 38){
-  player2.dir = 0
-
-  }
- else if(e.keyCode === 40){
-  player2.dir = 0
- }
-
-
-
-})
-
-
-
 function moverPlayer2(){
   if(player2.py <0){
     player2.py = 0 
@@ -117,7 +103,6 @@ function moverPlayer2(){
   }
   player2.py += player2.dir
 }
-
 
 
 function moverBolinha(){
@@ -134,19 +119,23 @@ function moverBolinha(){
 
 }
 
+
 function fimJogo(){
    if(pts1 > 2 || pts2 > 2){
   jogar = false
   }
 }
 
+
 function colisaoBolinha(){
   if(bolinha.py + bolinha.largura >= player2.py && bolinha.py <= player2.py +player2.largura && bolinha.px <= player2.px + player2.tamanho && bolinha.px >= player2.px - player2.tamanho){
     bolinha.dir *= -1
+    audio.play()
  }
 
  else if(bolinha.py + bolinha.largura >= player1.py && bolinha.py <= player1.py + player1.largura && bolinha.px <= player1.px + player1.tamanho && bolinha.px >= player1.px - player1.tamanho){
     bolinha.dir *= -1
+    audio.play()
  }
 }
 
@@ -165,39 +154,36 @@ function pontos(){
   }
 }
 
-function draw(){
+function Draw(){
     quadro.fillRect(player1.px,player1.py,player1.tamanho,player1.largura)
     quadro.fillRect(player2.px,player2.py,player2.tamanho,player2.largura)
     quadro.fillRect(bolinha.px,bolinha.py,bolinha.tamanho,bolinha.largura)
     quadro.fillText(`pontos1 : ${pts1}`,280,50)
-    quadro.fillText(`Pontos2 : ${pts2}`,850,50)
-    
+    quadro.fillText(`Pontos2 : ${pts2}`,850,50)   
 }
 
 function talVencedor(){
   quadro.clearRect(0,0,1280,720) 
   quadro.font = '60px arial'
   quadro.fillText( `score1 : ${pts1}`, 200,345)
-  quadro.fillText(`score 1 : ${pts2}`, 800,345)
-
-
-
-  
+  quadro.fillText(`score 1 : ${pts2}`, 800,345) 
 }
+
 function main(){
   if(jogar){
     quadro.clearRect(0,0,1280,720) // limpar todo o meu objeto canvas
-    draw() // redesenhar
-    moverBolinha()
-    colisaoBolinha()
-    pontos()
+    Draw() // redesenhar  
     moverPlayer1()
     moverPlayer2()
+    moverBolinha()   
+    colisaoBolinha()
+    pontos()
     fimJogo()
   } 
 else{
-  draw()
+  talVencedor()
+}
 }
 
 setInterval(main, 10)
-}
+
